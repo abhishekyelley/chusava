8b9abe1b4c316916441971e464659ffd4344f934
Abhishek Yelley <abhishekyelley@gmail.com>
2024-10-06 22:35:34 +0530

    add: dropdown menu for list

--- components/chats/list-card.tsx
+++ components/chats/list-card.tsx
@@ -7,9 +7,16 @@ import {
 } from "@/components/ui/tooltip";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { Library } from "lucide-react";
+import { Library, MoreVertical, Pencil, Share } from "lucide-react";
 import moment from "moment";
 import { paths } from "@/lib/constants";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
 
 export function ListCard({
   listId,
@@ -28,22 +35,17 @@ export function ListCard({
   return (
     <TooltipProvider delayDuration={50}>
       <Tooltip>
-        <TooltipTrigger
-          className="my-2"
-          id={listId}
-          asChild
+        <div
+          className={cn(
+            "flex justify-between",
+            "rounded-md px-3 py-1 my-2 border-muted border",
+            "hover:cursor-pointer hover:bg-muted/50",
+            "transition-all ease-in-out duration-300",
+            pathname.includes(listId) ? "bg-muted/50" : ""
+          )}
         >
-          <Link
-            href={`${paths.chats}/${conversationId}/${listId}`}
-          >
-            <div
-              className={cn(
-                "rounded-md px-3 py-1 my-2 border-muted border",
-                "hover:cursor-pointer hover:bg-muted",
-                "transition-all ease-in-out duration-300",
-                pathname.includes(listId) ? "bg-muted" : ""
-              )}
-            >
+          <TooltipTrigger className="my-2" asChild>
+            <Link href={`${paths.chats}/${conversationId}/${listId}`} className="w-full mr-2">
               <div className="flex items-center">
                 <div className="w-full transition-all ease-in-out duration-300 z-10">
                   <div className="flex justify-between items-center">
@@ -64,10 +66,27 @@ export function ListCard({
                   </div>
                 </div>
               </div>
-            </div>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="right">
+            </Link>
+          </TooltipTrigger>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <MoreVertical className="h-4 w-4" />
+                <span className="sr-only">More</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>
+                <Pencil className="h-4 w-4 mr-2" /> Rename
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Share className="h-4 w-4 mr-2" /> Share
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+
+        <TooltipContent side="top">
           <>
             <p className="font-bold italic">
               {listNameinConversation}
